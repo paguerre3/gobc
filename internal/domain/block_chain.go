fecdd0a0ee6da3c9ebc914e74f12ee41d44648d5
paguerre3 <camila.milagros.aguerre@gmail.com>
2024-10-22 12:19:24 -0300

    init bc code

--- internal/domain/block_chain.go
+++ internal/domain/block_chain.go
@@ -2,6 +2,19 @@ package domain
 
 import "time"
 
+type Block interface {
+	Nonce() int
+	PreviousHash() string
+	TimeStamp() time.Time
+	Transactions() []string
+}
+
+type BlockChain interface {
+	TransactionPool() []string
+	Chain() []Block
+	CreateBlock(nonce int, previousHash string) Block
+}
+
 type block struct {
 	nonce        int
 	prevHash     string
@@ -11,14 +24,51 @@ type block struct {
 
 type blockChain struct {
 	transactionPool []string
-	chain           []*block
+	chain           []Block
 }
 
-func newBlock(nonce int, prevHash string) *block {
+func newBlock(nonce int, previousHash string) Block {
 	return &block{
 		nonce:     nonce,
-		prevHash:  prevHash,
+		prevHash:  previousHash,
 		timeStamp: time.Now(),
 		//transactions: transactions,
 	}
+}
+
+func (b *block) Nonce() int {
+	return b.nonce
+}
+
+func (b *block) PreviousHash() string {
+	return b.prevHash
+}
+
+func (b *block) TimeStamp() time.Time {
+	return b.timeStamp
+}
+
+func (b *block) Transactions() []string {
+	return b.transactions
+}
+
+func NewBlockchain() BlockChain {
+	bc := new(blockChain)
+	//bc.transactionPool = []string{"Genesis transaction"}
+	bc.CreateBlock(0, "Genesis block")
+	return bc
+}
+
+func (bc *blockChain) TransactionPool() []string {
+	return bc.transactionPool
+}
+
+func (bc *blockChain) Chain() []Block {
+	return bc.chain
+}
+
+func (bc *blockChain) CreateBlock(nonce int, previousHash string) Block {
+	b := newBlock(nonce, previousHash)
+	bc.chain = append(bc.chain, b)
+	return b
 }
