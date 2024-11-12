6c449da7e0d323f851235c5745797eb2932de11e
paguerre3 <camila.milagros.aguerre@gmail.com>
2024-11-12 12:52:28 -0300

    init server

---
+++ cmd/gobc/server_node.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type ServerNode interface {
+	Run()
+}
+
+type serverNode struct {
+	port string
+}
+
+func newServerNode(port string) ServerNode {
+	return &serverNode{port: port}
+}
+
+func (s *serverNode) Run() {
+	// Initialize a new Gin router
+	router := gin.Default()
+
+	// Define a simple GET route
+	router.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+
+	// Start the server on port 8080
+	router.Run(s.port)
+}
