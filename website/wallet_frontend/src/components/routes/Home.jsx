4cf86b8a612632a46b0d3216327d12430b145f73
paguerre3 <camila.milagros.aguerre@gmail.com>
2024-11-23 22:48:47 -0300

    style fixes

--- website/wallet_frontend/src/components/routes/Home.jsx
+++ website/wallet_frontend/src/components/routes/Home.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
-import { BrowserRouter as Router, Routes, Route, useNavigate } from "react-router-dom";
+import { useNavigate } from 'react-router-dom'
 
 export default function Home() {
   const navigate = useNavigate();
   return (
     <div className="container">
-        <button className="button" onClick={() => navigate("/wallet") }>Get Started</button>
+        <button className="button" onClick={() => navigate('/wallet')}>Get Started</button>
         <div className="features">
-            <p><h2>Secure Storage</h2> Keep your private keys safe with industry-leading encryption.</p>
-            <p><h2>Easy Transactions</h2> Send and receive crypto with a seamless user interface.</p>
-            <p><h2>Real-Time Tracking</h2> Monitor live prices and track your portfolio performance.</p>
-            <p><h2>Decentralized Access</h2> Full control over your wallet—no third parties involved.</p>
+            <div><h2>Secure Storage</h2> Keep your private keys safe with industry-leading encryption.</div>
+            <div><h2>Easy Transactions</h2> Send and receive crypto with a seamless user interface.</div>
+            <div><h2>Real-Time Tracking</h2> Monitor live prices and track your portfolio performance.</div>
+            <div><h2>Decentralized Access</h2> Full control over your wallet—no third parties involved.</div>
         </div>
     </div>
   )
--- website/wallet_frontend/src/components/routes/Wallet.jsx
+++ website/wallet_frontend/src/components/routes/Wallet.jsx
@@ -5,24 +5,24 @@ export default function Wallet() {
     <div className='container'>
       <button className="button">Reload Wallet</button>
       <div className='features'>
-        <p><h2>Wallet</h2></p>
+        <h2>Wallet</h2>
         <h2><div id="walletBalance">0</div></h2>
 
-        <p><strong>Public Key</strong></p>
-        <textarea id="publicKey" cols="73" rows="2"></textarea>
+        <strong>Public Key</strong>
+        <textarea id="publicKey" cols="73" rows="2"/>
 
-        <p><strong>Private Key</strong></p>
-        <textarea id="privateKey" cols="73" rows="1"></textarea>
+        <strong>Private Key</strong>
+        <textarea id="privateKey" cols="73" rows="1"/>
 
-        <p><strong>Blockchain Address</strong></p>
-        <textarea id="blockChainAddress" cols="73" rows="1"></textarea>
+        <strong>Blockchain Address</strong>
+        <textarea id="blockChainAddress" cols="73" rows="1"/>
       </div>
       <div className='features'>
-        <p><h2>Send Money</h2></p>
+        <h2>Send Money</h2>
         <h2>Amount <input id="sendAmount" type="number"/></h2>
 
-        <p><strong>Receipient Blockchain Address</strong></p>
-        <textarea id="receipientBlockChainAddress" cols="73" rows="1"></textarea>
+        <strong>Recipient Blockchain Address</strong>
+        <textarea id="recipientBlockChainAddress" cols="73" rows="1"/>
       </div>
       <h2><button className="button">Send</button></h2>
     </div>
